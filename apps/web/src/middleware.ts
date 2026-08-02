bc4b3f3802b14e0b60bfeda8efc36d1155f75987
Isaac <seuemail@gmail.com>
2026-08-02 19:27:39 -0300

    fix: liberar asset da logo no login

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -8,4 +8,4 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = { matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icon.svg).*)"] };
+export const config = { matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icon.svg|telun-logo.jpeg).*)"] };
